9d29df8b26895af2ee7274dbeda65466b120c07a
MahmoudEMan <69572998+MahmoudEMan@users.noreply.github.com>
2023-08-06 03:46:45 +0300

    M

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+"use client";
+import { getProviders, signIn } from "next-auth/react";
+import { useState, useEffect } from "react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
 
 export default function AuthProviders() {
+  const [providers, setProviders] = useState<Providers | null>(null);
   return <div>AuthProviders</div>;
 }
